dbebad82a9984c3b8f3b5c17578deae48e23b5b1
AlexVoid14 <117752680+AlexVoid14@users.noreply.github.com>
2023-06-21 18:45:46 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
-from models.state import State
+from models.review import Review
+import os
 
 
-class test_state(test_basemodel):
-    """ """
+class test_review(test_basemodel):
+    """ review test class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ review class init"""
         super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+        self.name = "Review"
+        self.value = Review
 
-    def test_name3(self):
-        """ """
+    def test_place_id(self):
+        """ testing review place_id attr"""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.place_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_user_id(self):
+        """ testing review user_id attr"""
+        new = self.value()
+        self.assertEqual(type(new.user_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_text(self):
+        """ testing review text attr"""
+        new = self.value()
+        self.assertEqual(type(new.text), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
