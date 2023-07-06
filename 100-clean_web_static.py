42b88c241c1039cdf7e9df786a8513487e90ae77
Nicktrusere <Nicktrusere@gmail.com>
2023-07-06 13:20:40 -0700

    updating web static files

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.24.72.129', '35.153.16.143']
 
 
 def do_clean(number=0):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,30 +1,60 @@
 #!/usr/bin/python3
-"""
-based file 1-pack_web_static.py and distributes an
-archive to the web servers
-"""
+from fabric.api import put, run, local, env
+from time import strftime
+from datetime import date
+from os import path
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['54.162.6.230', '3.84.168.250']
+env.hosts = ["100.24.72.129", "35.153.16.143"]
+
+
+def do_pack():
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
+        return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Fabric script that distributes
+    an archive to your web server"""
+
+    if not path.exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        tgzfile = archive_path.split("/")[-1]
+        print(tgzfile)
+        filename = tgzfile.split(".")[0]
+        print(filename)
+        pathname = "/data/web_static/releases/" + filename
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgzfile, filename))
+        run("rm /tmp/{}".format(tgzfile))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
         return True
-    except:
+    except Exception as e:
         return False
+
+
+def deploy():
+    """run the 2 functions"""
+
+    path = do_pack()
+    if not path:
+        return False
+
+    return do_deploy(path)
